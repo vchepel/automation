4855a479907a9508c7801676a2b8ee9905c3c6da
vchepel <vchepel@playtika.com>
2019-02-08 19:09:23 +0200

    Third commit

--- src/main/java/com/playtika/automation/CandyShop.java
+++ src/main/java/com/playtika/automation/CandyShop.java
@@ -7,20 +7,25 @@ public class CandyShop {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
+
         System.out.println("1kg Candies price: ");
         float candy = console.nextFloat();
 
         System.out.println("1kg Cookies price: ");
         float cookies = console.nextFloat();
 
-        //float orderPrice = 0.3f * candy + 0.4f * cookies;
-
-        //System.out.println("Summary = " + orderPrice);
-        System.out.println("Summary = " + calculatedPrice(candy, cookies));
+        System.out.println("Summary for one order = " + calculatedPrice(candy, cookies));
+        System.out.println("Summary for three orders = " + calculatedSecondPrice(candy, cookies));
     }
+
     public static float calculatedPrice(float candiesKgPrice, float cookiesKgPrice){
         float orderPrice = 0.3f * candiesKgPrice + 0.4f * cookiesKgPrice;
         return orderPrice;
 
+    }
+
+    public static float calculatedSecondPrice(float moreCandies, float moreCookies){
+        float secondOrderPrice = (1.8f * moreCandies + 2f * moreCookies) * 3;
+        return secondOrderPrice;
     }
 }
